345b4b803a8e390f77e6f3cde161827e3b52c14e
nateybear123 <nate@schwartzboys.com>
2026-02-17 18:15:48 -0500

    tdfg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class motorTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        DcMotor backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        DcMotor backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if(gamepad1.a) {
+                frontLeft.setPower(.1);
+                frontRight.setPower(.1);
+                backLeft.setPower(.1);
+                backRight.setPower(.1);
+            }
+
+        }
+    }
+
+
+}
