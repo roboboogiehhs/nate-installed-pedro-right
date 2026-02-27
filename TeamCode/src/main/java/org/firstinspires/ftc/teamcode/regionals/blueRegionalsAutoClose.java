3bc740cb3f3688373df32112d7cb178ff29113fd
reversered2151 <thedrwholaughs@gmail.com>
2026-02-27 17:54:42 -0500

    autio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/regionals/blueRegionalsAutoClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/regionals/blueRegionalsAutoClose.java
@@ -38,8 +38,8 @@ public class blueRegionalsAutoClose extends NextFTCOpMode {
     Pose pickupClassifier = new Pose(13, 55, Math.toRadians(155));
     Pose pickupRow1 = new Pose(12, 76, Math.toRadians(180));
     Pose pickupRow3 = new Pose(12, 27, Math.toRadians(180));
-    Pose offLineLaunch = new Pose(66, 99, Math.toRadians(140));
-    Pose offLineTurn = new Pose(56, 92, Math.toRadians(0));
+    Pose offLineLaunch = new Pose(55, 91, Math.toRadians(140));
+    Pose offLineTurn = new Pose(55, 91, Math.toRadians(0));
 
     PathChain scorePreload;
     PathChain grabRow2;
