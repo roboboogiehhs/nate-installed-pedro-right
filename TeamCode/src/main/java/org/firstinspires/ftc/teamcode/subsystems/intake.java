55de4b08ae64282729be02ee974e537f07b1e96e
nateybear123 <nate@schwartzboys.com>
2026-02-28 00:40:27 -0500

    sdfsdfsdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
@@ -1,23 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.ftc.ActiveOpMode;
 
 public class intake implements Subsystem {
     public static final intake INSTANCE = new intake();
     private intake() { }
 
-    private MotorEx motor = new MotorEx("intake")
-            .reversed();
+    private DcMotorEx motor;
 
     private boolean isOn = false;
 
-    public Command turnOn(double power) {
+    @Override
+    public void initialize() {
+        motor = ActiveOpMode.hardwareMap().get(DcMotorEx.class, "intake");
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+    }
+
+    public Command turnOn(double velocity) {
         return new LambdaCommand("Intake On")
                 .setStart(() -> {
-                    motor.setPower(power);
+                    motor.setVelocity(velocity);
                     isOn = true;
                 })
                 .setIsDone(() -> true)
@@ -25,13 +33,13 @@ public class intake implements Subsystem {
     }
 
     public Command turnOn() {
-        return turnOn(.8);
+        return turnOn(2400);
     }
 
     public Command turnOff() {
         return new LambdaCommand("Intake Off")
                 .setStart(() -> {
-                    motor.setPower(0);
+                    motor.setVelocity(0);
                     isOn = false;
                 })
                 .setIsDone(() -> true)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/uptake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/uptake.java
@@ -1,31 +1,39 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.ftc.ActiveOpMode;
 
 public class uptake implements Subsystem {
     public static final uptake INSTANCE = new uptake();
     private uptake() { }
 
-    private MotorEx motor = new MotorEx("uptake").brakeMode();
+    private DcMotorEx motor;
 
+    @Override
+    public void initialize() {
+        motor = ActiveOpMode.hardwareMap().get(DcMotorEx.class, "uptake");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+    }
 
-    public Command turnOn(double power) {
+    public Command turnOn(double velocity) {
         return new LambdaCommand("Uptake On")
-                .setStart(() -> motor.setPower(power))
+                .setStart(() -> motor.setVelocity(velocity))
                 .setIsDone(() -> true)
                 .requires(this);
     }
 
     public Command turnOn() {
-        return turnOn(0.8);
+        return turnOn(1400);
     }
 
     public Command turnOff() {
         return new LambdaCommand("Uptake Off")
-                .setStart(() -> motor.setPower(0))
+                .setStart(() -> motor.setVelocity(0))
                 .setIsDone(() -> true)
                 .requires(this);
     }
